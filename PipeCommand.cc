9dc2007e1c5ecf2f933547cfe0b34aa0d2a00360
Vikhyat Sai Jagini <vjagini@data.cs.purdue.edu>
2024-02-27 10:49:59 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -115,7 +115,7 @@ void PipeCommand::execute() {
     }
 
     // Print contents of PipeCommand data structure
-    print();
+    // print();
 
     // Add execution here
     // For every simple command fork a new process
