4b071e397fbb1d3dea0bb1ffe2b4a47620fc9b1b
Vikhyat Sai Jagini <vjagini@borg01.cs.purdue.edu>
2024-04-09 11:57:28 -0400

    Commit

--- Shell.cc
+++ Shell.cc
@@ -36,7 +36,7 @@ extern "C" void ctrlC(int sig) {
 void Shell::prompt() {
     if (_enablePrompt) {
       std::string prmpt = getenv("PROMPT");
-      if(prmptv == NULL) {
+      if(prmpt == NULL) {
         printf(prmpt);
       } else {
 	      printf("myshell>");
