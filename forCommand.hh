fd7f636eb9570a8fa30c79be081bf1282517dce6
Vikhyat Sai Jagini <vjagini@borg01.cs.purdue.edu>
2024-04-08 23:26:04 -0400

    Commit

--- forCommand.hh
+++ forCommand.hh
@@ -11,7 +11,7 @@ class forCommand : public Command {
 public:
   SimpleCommand * _condition;
   ListCommands * _listCommands; 
-  std::string word;
+  std::string * word;
   vector<std::string> args;
 
   forCommand();
